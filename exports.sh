08ab2d2c1101e21542f63a2d8e2f2fdd78f33b5c
Ruben <2327763+rubofvil@users.noreply.github.com>
2024-03-26 17:17:44 +0100

    Change how is showing the Prompt line

--- exports.sh
+++ exports.sh
@@ -12,3 +12,8 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 PATH="$(composer config -g home)/vendor/bin:$PATH"
+
+PROMPT='%{$fg[green]%}%~%{$fg_bold[blue]%}$(git_prompt_info)%{$reset_color%} '
+
+## Get the time
+RPROMPT='%{$fg[green]%}[%D{%H:%M:%S}]%{$reset_color%}'
